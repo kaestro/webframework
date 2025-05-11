c991d816a15f1ecac4c082c3e3b3936d2db0c43a
kaestro <didme07@gmail.coom>
2025-05-11 15:01:14 +0900

    feat: 기본 요청 수신 모듈 구현
    - engine에 listener 추가
    - engine.Start() 함수 추가
    - engine.Stop() 함수 추가
    - engine.PrintRequest() 함수 추가
    - basic-client 예제 추가

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+
+	webframework "github.com/kaestro/webframework/src"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	engine := webframework.NewEngine()
+	listener, err := engine.Start(webframework.DefaultAddress)
+	if err != nil {
+		fmt.Println("Error starting engine:", err)
+		return
+	}
+	fmt.Println("Server listening on", webframework.DefaultAddress)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go func(c net.Conn) {
+			engine.PrintRequest(c)
+		}(conn)
+	}
 }
